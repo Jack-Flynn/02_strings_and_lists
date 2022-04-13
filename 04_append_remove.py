5b426d3ff53da3231685b16f9fff955f68091e1d
Jack-Flynn <flynn.jack@awatapu.school.nz>
2022-04-14 10:54:54 +1200

    append remove with actual code now

--- 04_append_remove.py
+++ 04_append_remove.py
@@ -3,3 +3,17 @@ shopping.append("eggs")
 shopping.append("flour")
 shopping.remove("cheese")
 print(shopping)
+
+while True:
+    edit = input("Would you like to edit your shopping list? Y/N").upper()
+    if edit == "Y":
+        edit = input("Would you like to add or remove an item? A/R").upper()
+        if edit == "A":
+            edit = input("Enter an item to add:").lower()
+            shopping.append(edit)
+        elif edit == "R":
+            edit = input("Enter an item to remove:").lower()
+            shopping.remove(edit)
+    elif edit == "N":
+        break
+print(shopping)
